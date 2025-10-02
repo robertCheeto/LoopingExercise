ee2a3509775a7e57151b14b2a99ec6211b27793b
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 09:56:54 -0400

    * added an exception to pause the countdown by a second before displaying the next number

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,8 +1,9 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 10; i > 0; i--) {
+            Thread.sleep(1000);
             System.out.println(i);
         }
         System.out.println("Launch!");
