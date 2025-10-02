de0c80a024a10266075d363fc719ae321a3041f0
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 09:55:45 -0400

    * created a ForLoop file to do a countdown sequence before a "Launch" message appears after 1 becomes 0

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Launch!");
+    }
+}
